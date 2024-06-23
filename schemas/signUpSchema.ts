5167f4ed9a6815dc3a548e221db032afbc404a37
rahul4019 <rahulkashyap4019@gmail.com>
2024-06-23 12:08:43 +0530

    Add check-uniqe-username route

--- schemas/signUpSchema.ts
+++ schemas/signUpSchema.ts
@@ -4,7 +4,7 @@ export const usernameValidation = z
     .string()
     .min(2,'Username must be at least 2 characters')
     .max(20, 'Username must be no more than 20 characters')
-    .regex(/^ [a-zA-Z0-9_]+$/, "Username must not contain special characters")
+    .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters")
 
 export const signUpSchema = z.object({
     username: usernameValidation,
