b64b595fa4cd44e26169fba496c0dffc95a06e5e
rahul4019 <rahulkashyap4019@gmail.com>
2024-06-23 09:00:44 +0530

    Add authprovider and wrapped layout with it

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+'use client';
+import { useSession, signIn, signOut } from "next-auth/react";
 
-function page() {
+export default function Component() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
   return (
-    <div>page</div>
-  )
+    <>
+      Not signed in <br />
+      <button className="shadow-[0_4px_14px_0_rgb(0,118,255,39%)] hover:shadow-[0_6px_20px_rgba(0,118,255,23%)] hover:bg-[rgba(0,118,255,0.9)] px-8 py-2 bg-[#0070f3] rounded-md text-white font-light transition duration-200 ease-linear" onClick={() => signIn()}>
+        Sign in
+      </button>
+    </>
+  );
 }
-
-export default page
