bc7d2e4110699ffe27d70d5eb9243a3503b7eb94
rahul4019 <rahulkashyap4019@gmail.com>
2024-08-04 19:16:12 +0530

    Add Signin page

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
+import '../globals.css'
 import { Inter } from "next/font/google";
-import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
 
